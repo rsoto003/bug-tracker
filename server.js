8950136ef3769f4f375f93cd8a0878c18a19062c
Ryan Soto <rsoto003@ucr.edu>
2019-06-24 00:04:34 -0700

    added endpoints to for get, post and delete requests

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
+const items = require('./routes/api/items');
 
 app.use(bodyParser.json());
 
@@ -11,6 +12,8 @@ const database = require('./config/keys').mongoURI;
 mongoose.connect(database)
     .then( () => console.log('MongoDB is up and running...'))
         .catch(err => console.log(err));
+
+app.use('/api/items', items);
 
 const port = process.env.port || 5000;
 
