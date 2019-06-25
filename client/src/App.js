520d49c1714abc4cdcb31c5283e415ec0a1e0f6c
Ryan Soto <rsoto003@ucr.edu>
2019-06-24 23:48:08 -0700

    created react application and added necessary dependencies for application.

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import AppNavbar from './components/AppNavbar'
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <AppNavbar />
+      </div>
+    );
+  }
+}
+
+export default App;
